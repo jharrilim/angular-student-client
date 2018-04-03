7fdbc6064bc3612a0a0a4083845b63023e9de727
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2018-04-02 20:04:05 -0400

    add proper routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SidebarModule } from 'ng-sidebar';
-
+import { RoutingModule } from './routing/routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { StudentsComponent } from './students/students.component';
-import { CoursesComponent } from './courses/courses.component';
+import { StudentsComponent } from './components/students/students.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +18,13 @@ import { CoursesComponent } from './courses/courses.component';
     SignupComponent,
     ProfileComponent,
     StudentsComponent,
-    CoursesComponent
+    CoursesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    SidebarModule.forRoot()
+    SidebarModule.forRoot(),
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
